a993e71b1f256eb9f7cd9fde1b2dba05ad0d5f58
kozerka <kozerka@gmail.com>
2024-01-17 18:22:21 +0100

    feat:recent post section added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,15 +7,15 @@ interface HeaderProps {
 
 const Header: React.FC<HeaderProps> = ({ title = '', subtitle = '' }) => {
 	return (
-		<header className="py-10 px-4 mb-12 text-center border-b dark:border-primary">
-			<h2 className="text-7xl font-extrabold text-orange-500 drop-shadow-lg mb-6">
-				{title}
-			</h2>
-			<p className="text-4xl font-bold dark:text-white drop-shadow-lg shadow-black">
-				{subtitle}
-			</p>
-		</header>
-	);
+    <header className="py-10 px-4  text-center border-b dark:border-primary">
+      <h2 className="lg:text-7xl  text-4xl font-extrabold text-orange-500 drop-shadow-lg mb-6">
+        {title}
+      </h2>
+      <p className="lg:text-4xl text-2xl font-bold dark:text-white drop-shadow-lg shadow-black">
+        {subtitle}
+      </p>
+    </header>
+  );
 };
 
 export default Header;
